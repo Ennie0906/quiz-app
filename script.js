13d4315d50a285ac08ff3889b7f8355d77a2ebed
Enes Arici <enesarici12@outlook.com>
2023-10-23 16:02:04 +0000

    added answers and question and started the JavaScript

---
+++ script.js
@@ -0,0 +1,128 @@
+const questions = [{
+        question: "What color is the sky?",
+        options: [{
+                text: "Blue",
+
+            },
+            {
+                text: "Green",
+
+            },
+            {
+                text: "Red",
+
+            },
+            {
+                text: "Yellow",
+
+            },
+        ],
+        correct: "Blue"
+
+
+    },
+    {
+        question: "What is the fastest car in the world?",
+        answers: [{
+                text: "Bugatti Veyron Super Sport",
+                correct: true
+            },
+            {
+                text: "Koenigsegg Jesko Absolut",
+                correct: false
+            },
+            {
+                text: "Hennessey Venom F5",
+                correct: false
+            },
+            {
+                text: "Mclaren P1",
+                correct: false
+            },
+        ]
+
+    },
+    {
+        question: "Who won 2010 FIFA World Cup?",
+        answers: [{
+                text: "Spain",
+                correct: true
+            },
+            {
+                text: "Brazil",
+                correct: false
+            },
+            {
+                text: "England",
+                correct: false
+            },
+            {
+                text: "West Ham United",
+                correct: false
+            },
+        ]
+
+    },
+
+    {
+        question: "Who would win in a fight between Richey and Enes?",
+        answers: [{
+                text: "Enes",
+                correct: true
+            },
+            {
+                text: "Richey",
+                correct: false
+            },
+            {
+                text: "Richey",
+                correct: false
+            },
+            {
+                text: "Richey",
+                correct: false
+            },
+        ]
+    },
+    {
+        question: "Who is the better cook between Richey and Enes?",
+        answers: [{
+                text: "Enes",
+                correct: true
+            },
+            {
+                text: "Richey",
+                correct: false
+            },
+            {
+                text: "Richey",
+                correct: false
+            },
+            {
+                text: "Richey",
+                correct: false
+            },
+        ]
+    },
+    {
+        question: "If Richey was 60 years old and Enes was 5 who would win a MMA Cage Fight?",
+        answers: [{
+                text: "Enes",
+                correct: true
+            },
+            {
+                text: "Richey",
+                correct: false
+            },
+            {
+                text: "Richey",
+                correct: false
+            },
+            {
+                text: "Richey",
+                correct: false
+            },
+        ]
+
+    }
+];
